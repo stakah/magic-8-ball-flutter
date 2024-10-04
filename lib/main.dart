1fd954ea1379dd9156c6e670c1d694a3c9f3895b
Sergio Takahashi <stakah@gmail.com>
2024-10-04 11:07:47 -0300

    Bump flutter version. Completed App.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        titleTextStyle: TextStyle(color: Colors.white),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      backgroundColor: Colors.blue,
+      body: Ball(
+        key: key,
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  var ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () => {
+            setState(() {
+              ballNumber = Random().nextInt(4) + 1;
+              print('$ballNumber');
+            })
+          },
+        ),
+      ),
+    );
+  }
+}
